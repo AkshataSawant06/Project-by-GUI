7fdc534bd108b9b7a9e98ed8f5ad78d3863cbc91
AkshataSawant06 <idakshatasawant@gmail.com>
2024-05-09 18:04:35 +0530

    Added implemented File

---
+++ GUIPROJECT-GITHUB/src/com/MathOperationUniml/Impl.java
@@ -0,0 +1,12 @@
+package com.MathOperationUniml;
+
+public class Impl implements Operation{
+
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		System.out.println(a+b);
+	}
+
+}
